4e76c51ebe0f5c03cd8b125f6c58d865c43426d8
SuperHotDogCat <siromisochan@gmail.com>
2024-04-17 01:34:57 +0900

    phase2: あまりを求める関数なので名前を変更した

--- arai60/zigzag_conversion/phase2.py
+++ arai60/zigzag_conversion/phase2.py
@@ -5,21 +5,21 @@
 # 何個か戻ったところに文字があるのでそれを回収してくる...のような方法でいこうとしたが, 頭がこんがらがってしまい, 他にいい方法がないかを考えてみた。
 # i行目にある文字は表現するためには, 文字列のj-index番目とnumRowsに関する何らかの余りで表現できることに気づいたので, それを用いた。
 
-def create_string_index_to_col_index(numRows: int)->dict:
-    string_index_to_col_index = {}
+def create_string_index_to_column_index(numRows: int)->dict:
+    string_index_to_column_index = {}
     for string_index in range(numRows):
-        col_index = string_index
-        string_index_to_col_index[string_index] = col_index
+        column_index = string_index
+        string_index_to_column_index[string_index] = column_index
         #{0: 0, 1: 1, 2: 2, ..., numRows - 1: numRows - 1}
     for difference_index in range(numRows - 2):
         string_index = numRows + difference_index
-        col_index = numRows - difference_index - 2
-        string_index_to_col_index[string_index] = col_index
+        column_index = numRows - difference_index - 2
+        string_index_to_column_index[string_index] = column_index
     # 周期2*numRows - 2で変化するindexの辞書ができた
     #{0: 0, 1: 1, 2: 2, ..., numRows - 1: numRows - 1, numRows: numRows - 2, ..., 2 * numRows - 3: 1}
-    return string_index_to_col_index
+    return string_index_to_column_index
 
-def create_col_index(index, numRows):
+def create_remain_string_index(index, numRows):
     if numRows == 1:
         return 0
     return index % (2 * numRows - 2)
@@ -27,12 +27,10 @@ def create_col_index(index, numRows):
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         concat_strings = ["" for _ in range(numRows)]
-        string_index_to_col_index = create_string_index_to_col_index(numRows)
+        string_index_to_column_index = create_string_index_to_column_index(numRows)
         for string_index, string in enumerate(s):
-            concat_strings[string_index_to_col_index[create_col_index(string_index, numRows)]] += string
+            concat_strings[string_index_to_column_index[create_remain_string_index(string_index, numRows)]] += string
         concated_string = ""
         for string in concat_strings:
             concated_string += string
         return concated_string
-
-print(create_string_index_to_col_index(3))
