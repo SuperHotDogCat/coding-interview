5faf7a81046562c09fed951285f1122e2d55142a
SuperHotDogCat <siromisochan@gmail.com>
2024-04-22 01:02:11 +0900

    phase1 説明追記

--- valid_parentheses/phase1.py
+++ valid_parentheses/phase1.py
@@ -1,3 +1,12 @@
+# in-validとなるとき
+# ある種類のopen branketの出現回数が0なのにclosed branketが出てくる
+# closed branketが出てきた時, open branketに他の種類のものが紛れ込んでいる
+# 全ての作業が終わった後, open branketの数が0になっていない
+
+# ↑...という回答を真っ先に思い浮かんだが, 何か考えすぎているきがした。
+# 明らかにおかしいのでもう少し問題の性質をちゃんと考えてみることに
+# dequeを使って最後に見たopen branketを管理するFIFOにすればいいことに気付き, 採用
+
 from collections import deque
 
 class Solution:
