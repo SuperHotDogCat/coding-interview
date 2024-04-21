079e91c6894f100595db79378f789e07f9755231
SuperHotDogCat <siromisochan@gmail.com>
2024-04-22 01:03:40 +0900

    phase2.py 名名変更, last seenなのがdequeの要素ではないから

---
+++ valid_parentheses/phase2.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        seen_open_brankets = deque([])
+        last_seen_open_branket = ""
+        open_brankets = set(["(", "[", "{"])
+        close_branket_to_open_branket = {")":"(", "}":"{", "]":"["}
+        for char in s:
+            if char in open_brankets:
+                seen_open_brankets.appendleft(last_seen_open_branket)
+                last_seen_open_branket = char
+            
+            elif char in close_branket_to_open_branket:
+                if last_seen_open_branket != close_branket_to_open_branket[char]:
+                    return False
+                if len(seen_open_brankets) != 0:
+                    last_seen_open_branket = seen_open_brankets.popleft()
+
+        if len(seen_open_brankets) != 0:
+            return False
+        
+        return True
