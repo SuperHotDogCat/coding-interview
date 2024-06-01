7a6b17246294437bbe74182e74bb17531dedda84
SuperHotDogCat <siromisochan@gmail.com>
2024-06-01 15:13:49 +0900

    名前をpyに合わせる

--- arai60/move_zeros/phase4.cpp
+++ arai60/move_zeros/phase4.cpp
@@ -3,14 +3,14 @@ public:
     void moveZeroes(vector<int>& nums) {
         auto result = nums.begin();
         auto last = nums.end();
-        int non_zero_count = 0;
+        int non_zero_length = 0;
         for (auto first = nums.begin();first != last; first++){
             if (!(*first == 0)){
                 *result++ = std::move(*first);
-                non_zero_count++;
+                non_zero_length++;
             }
         }
-        for (; non_zero_count < nums.size(); non_zero_count++){
+        for (; non_zero_length < nums.size(); non_zero_length++){
             *result++ = 0;
         }
     }
