f92fb213f4338fd9ef6fe9e748aec7c245580d38
SuperHotDogCat <siromisochan@gmail.com>
2024-12-24 10:52:50 +0900

    下からの再帰で解いた

--- validate_binary_tree/phase5.py
+++ validate_binary_tree/phase5.py
@@ -3,21 +3,29 @@ class Solution:
         if not root:
             return True
 
-        left_max, _ = self.subtreeMinMax(root.left)
-        _, right_min = self.subtreeMinMax(root.right)
+        left_max, left_min = self.subtreeMinMax(root.left)
+        right_max, right_min = self.subtreeMinMax(root.right)
 
-        if not (left_max < root.val < right_min):
+        if left_max is not None and left_max >= root.val:
             return False
-    
+        if right_min is not None and right_min <= root.val:
+            return False
+
         return self.isValidBST(root.left) and self.isValidBST(root.right)
     
     @cache
-    def subtreeMinMax(self, node: Optional[TreeNode]) -> Union[float, int]:
-        # nodeを含む部分木のmin, maxを出力する
+    def subtreeMinMax(self, node: Optional[TreeNode]) -> Tuple[Optional[int], Optional[int]]:
         if not node:
-            return -math.inf, math.inf
+            return None, None
 
         left_max, left_min = self.subtreeMinMax(node.left)
         right_max, right_min = self.subtreeMinMax(node.right)
 
-        return max(node.val, left_max, right_max), min(node.val, left_min, right_min)
+        if left_max is not None and right_max is not None:
+            return max(node.val, left_max, right_max), min(node.val, left_min, right_min)
+        if left_max is not None:
+            return max(node.val, left_max), min(node.val, left_min)
+        if right_max is not None:
+            return max(node.val, right_max), min(node.val, right_min)
+
+        return node.val, node.val
